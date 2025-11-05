1da932b021e5d829d432f47e37946632d99d2438
Miska Malkamaki <mitamalk.jyu.fi>
2025-11-05 17:54:10 +0200

    e2e-tests are ready

--- e2e-tests/e2e-tests.spec.js
+++ e2e-tests/e2e-tests.spec.js
@@ -6,4 +6,16 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('pokemon page can be opened', async ({ page }) => {
+    await page.goto('')
+    await page.locator('xpath=/html/body/div/div/a[1]').click()
+
+
+    await expect(page.getByText('Speed')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+    await expect(page.getByText('overgrow')).toBeVisible()
+
+
+  })
 })
